a83749e0e80e4aecebf43a0d5df8e4d09dd40df1
Jake Franklin <jacob.d.franklin@gmail.com>
2022-03-20 14:09:32 -0700

    Added build script for GH page

---
+++ build_gh_page.bat
@@ -0,0 +1,13 @@
+git checkout master
+git push origin master
+git checkout gh-pages
+git checkout origin/master dist/bundle.js
+git checkout origin/master public/index.html
+move /y dist/bundle.js bundle.js
+move /y public/index.html index.html
+del /q dist public
+git add .
+git commit -m "Latest production build"
+git push origin gh-pages
+git checkout master
+PAUSE
