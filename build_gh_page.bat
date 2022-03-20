df765c99b5016e641dff62a8ba8371613c60ead1
Jake Franklin <jacob.d.franklin@gmail.com>
2022-03-20 16:15:12 -0700

    updating build script

--- build_gh_page.bat
+++ build_gh_page.bat
@@ -1,13 +1,14 @@
 git checkout master
 git push origin master
-git checkout gh-pages
-git checkout origin/master dist/bundle.js
+git stash
+git checkout "gh-pages"
 git checkout origin/master public/index.html
-move /y dist/bundle.js bundle.js
-move /y public/index.html index.html
-del /q dist public
+git checkout origin/master dist/bundle.js
+move /y public\index.html index.html
+rmdir /s /q public
 git add .
-git commit -m "Latest production build"
-git push origin gh-pages
+git commit -m "Latest build"
+git push origin "gh-pages"
 git checkout master
+git stash pop
 PAUSE
