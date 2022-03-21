cc771cc791d7b9d1521cd0fe85f9f7e0b2c2f8d9
Jake Franklin <jacob.d.franklin@gmail.com>
2022-03-20 20:55:13 -0700

    udpate build script

--- build_gh_page.bat
+++ build_gh_page.bat
@@ -2,6 +2,7 @@ git checkout master
 git push origin master
 git stash
 git checkout "gh-pages"
+git checkout master -- build_gh_page.bat
 git checkout origin/master public/index.html
 git checkout origin/master dist/bundle.js
 move /y public\index.html index.html
