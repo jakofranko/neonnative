0866887ad50a800fe0cf3dd5b9fc57911df4d847
Jake Franklin <jacob.d.franklin@gmail.com>
2022-03-20 20:47:15 -0700

    ported build script

---
+++ build_gh_page.bat
@@ -0,0 +1,7 @@
+git checkout master
+git checkout "gh-pages"
+git checkout origin/master public/index.html
+git checkout origin/master dist/bundle.js
+move /y public\index.html index.html
+rmdir /s /q public
+PAUSE
